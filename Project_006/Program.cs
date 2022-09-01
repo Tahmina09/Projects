181e4b8ac61c1ff6785a4089d7b62949c4e8018f
Takhmina <tahminakhalilova9@gmail.com>
2022-09-01 18:44:25 +0500

    Project_006 Square

---
+++ Project_006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа");
+int x=Convert.ToInt32(Console.ReadLine());
+int y=Convert.ToInt32(Console.ReadLine());
+if(x==y*y)
+{
+   Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
