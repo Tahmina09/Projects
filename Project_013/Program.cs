d5dd3d78181b067c576f169c265395619c0cd23d
Takhmina <tahminakhalilova9@gmail.com>
2022-09-02 13:55:28 +0500

    Project_013 Остаток от деления

---
+++ Project_013/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+double num1= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double num2= Convert.ToInt32(Console.ReadLine());
+
+if (num1%num2==0)
+{
+    Console.WriteLine("Кратно.");
+}
+else
+{
+    Console.WriteLine("Не кратно. остаток-"+ (num1 % num2));
+}
