66b28c6405bcf17cb7faa2aaac6b67ae27a21d44
Takhmina <tahminakhalilova9@gmail.com>
2022-09-05 01:05:47 +0500

    Project_015 Наибольшая цифра случайного числа

---
+++ Project_015/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int number= new Random().Next(10,100);
+Console.WriteLine(number);
+int n1=number/10;
+int n2=number%10;
+if(n1>n2)
+{
+    Console.WriteLine(n1);
+}
+else
+{
+    Console.WriteLine(n2);
+}
