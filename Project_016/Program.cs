e166e7652d1552d220595d153d05e8297bf7c75b
Takhmina <tahminakhalilova9@gmail.com>
2022-09-06 01:47:06 +0500

    Project_016 верно одно из условий

---
+++ Project_016/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int x = Convert.ToInt32(Console. ReadLine());
+int y = Convert.ToInt32(Console. ReadLine());
+
+if (x==y*y || y==x*x)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
