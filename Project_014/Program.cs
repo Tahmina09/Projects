e6ca61aa665e48a8ba8423afb882d435dcbe97e0
Takhmina <tahminakhalilova9@gmail.com>
2022-09-06 01:25:44 +0500

    Project_014 2 варианта верны

---
+++ Project_014/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num % 7==0 && num % 23==0)
+{
+    
+    Console.WriteLine("ДА!");
+
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
