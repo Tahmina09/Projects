319ef829b088d943738aef879c13f3c46d30d8da
Takhmina <tahminakhalilova9@gmail.com>
2022-09-06 13:59:46 +0500

    Project_018 Четверть и диапозон-2

--- Project_018/Program.cs
+++ Project_018/Program.cs
@@ -1,22 +1,26 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
-
+Console.Clear();
 Console.WriteLine("Введите четверть координат:");
 int quater= Convert.ToInt32(Console.ReadLine());
 if (quater==1)
 {
     Console.WriteLine("x>=0; y>=0");
 }
-if (quater==2)
+else if (quater==2)
 {
     Console.WriteLine("x<=0; y>=0");
 }
-if (quater==3)
+else if (quater==3)
 {
     Console.WriteLine("x<=0; y<=0");
 }
-if (quater==4)
+else if (quater==4)
 {
     Console.WriteLine("x>=0; y<=0");
+}
+else
+{
+    Console.WriteLine("Такой четверти нет!");
 }
