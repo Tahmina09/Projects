5c1553e3f06f8e44f7a5672acab14c768adef6bd
Takhmina <tahminakhalilova9@gmail.com>
2022-09-06 13:52:46 +0500

    Project_020 квадраты чисел до n

---
+++ Project_020/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+int i=1;
+while(i<=n)
+{
+    double square= Math.Pow(i,2);
+    Console.WriteLine(square);
+    i++;
+}
+
