59fc940fe504b9f43ed02e826f6f495d053a0b59
Takhmina <tahminakhalilova9@gmail.com>
2022-09-06 13:34:08 +0500

    Project_019 Расстояние между точками

---
+++ Project_019/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты А:");
+Console.WriteLine("Введите X:");
+int xa= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+int ya= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты B:");
+Console.WriteLine("Введите X:");
+int xb= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+int yb= Convert.ToInt32(Console.ReadLine());
+
+int A= (xb-xa)*(xb-xa);
+int B= (yb-ya)*(yb-ya);
+int AB= A+B;
+double distance= Math.Sqrt(AB);
+distance=Math.Round(distance,3);
+Console.WriteLine(distance);
