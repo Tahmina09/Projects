96720301ae4404804bd56ce969845a932ce2eb8b
Тakhmina <tahminakhalilova9@gmail.com>
2022-09-09 13:33:39 +0500

    Project_023 Sum+for

---
+++ Project_023/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32 (Console.ReadLine());
+int multiply=1;
+for (int i=1; i<=N; i++)
+{
+    multiply*=i;
+}
+Console.WriteLine(multiply);
