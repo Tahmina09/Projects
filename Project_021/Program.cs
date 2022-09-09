e851075abf9fd454b30bdb5d5600237ded8aca68
Тakhmina <tahminakhalilova9@gmail.com>
2022-09-09 13:17:28 +0500

    Project_021 Sum+for

---
+++ Project_021/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+for (int i=1; i<=A; i++)
+{
+    sum+=i;
+}
+ Console.WriteLine(sum);
