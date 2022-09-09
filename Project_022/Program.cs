76f9911e69b71e740af48e42a9d8f5b848d5d5f7
Тakhmina <tahminakhalilova9@gmail.com>
2022-09-09 13:18:56 +0500

    Project_022 Количество цифр в числе

---
+++ Project_022/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int count = 0;
+for (; N!=0; count++)
+{
+    N/=10;
+}
+Console.WriteLine(count);
